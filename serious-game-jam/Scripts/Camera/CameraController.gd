c4cef28a83be8aef1d2eebc1672a4fc6927adfcc
sytsndev <sytsndev@gmail.com>
2026-06-20 19:09:58 -0400

    Add hovering shop items and them hovering

--- serious-game-jam/Scripts/Camera/CameraController.gd
+++ serious-game-jam/Scripts/Camera/CameraController.gd
@@ -6,12 +6,25 @@ extends Camera3D
 @export var shop_cam_pos: Marker3D
 
 var curr_camera: String
+var curr_hover_item: ShopItem3D = null
+var last_hover_area: Area3D = null
+var original_hover_scale: Vector3 = Vector3.ONE
+
+@export var hover_offset: float = 0.02
+@export var hover_scale: float = 1.01
+
+var ray_length: float = 100.0
 
 
 func _ready() -> void:
 	global_position = chair_cam_pos.global_position
 	global_rotation = chair_cam_pos.global_rotation
 	curr_camera = "chair"
+
+
+func _input(event):
+	if event is InputEventMouseMotion or event is InputEventMouseButton:
+		_update_ray_and_check_collision()
 
 
 func swap_camera(camera_pos: String):
@@ -33,3 +46,63 @@ func move_camera(pos: Vector3, rot: Vector3):
 	tween.set_parallel(true)
 	tween.tween_property(self, "global_position", pos, .5)
 	tween.tween_property(self, "global_rotation", rot, .5)
+
+
+func _update_ray_and_check_collision():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_origin = project_ray_origin(mouse_pos)
+	var ray_normal = project_ray_normal(mouse_pos)
+	var ray_end = ray_origin + ray_normal * ray_length
+
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+
+	query.collide_with_bodies = true	
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+
+	var new_hover_area: Area3D = null
+
+	if not result.is_empty():
+		var collider = result.collider
+		if collider is Node3D:
+			var obj: Node3D = collider
+			if obj is Area3D:
+				new_hover_area = obj
+			elif obj.get_parent() is Area3D:
+				new_hover_area = obj.get_parent() as Area3D
+
+	if new_hover_area and new_hover_area != last_hover_area:
+		if last_hover_area and curr_hover_item:
+			reset_hover_item()
+
+		last_hover_area = new_hover_area
+		curr_hover_item = new_hover_area.get_parent()
+		if curr_hover_item:
+			hover_item()
+
+	elif not new_hover_area and last_hover_area:
+		if curr_hover_item:
+			reset_hover_item()
+		curr_hover_item = null
+		last_hover_area = null
+
+
+func hover_item():
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_LINEAR)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	var pos = Vector3(curr_hover_item.global_position.x, curr_hover_item.global_position.y + hover_offset, curr_hover_item.global_position.z)
+	original_hover_scale = curr_hover_item.scale
+	tween.set_parallel(true)
+	tween.tween_property(curr_hover_item, "global_position", pos, 0.1)
+
+
+func reset_hover_item():
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_LINEAR)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	var pos = Vector3(curr_hover_item.global_position.x, curr_hover_item.global_position.y - hover_offset, curr_hover_item.global_position.z)
+	tween.set_parallel(true)
+	tween.tween_property(curr_hover_item, "global_position", pos, 0.1)
---
+++ serious-game-jam/Scripts/Shop/3D Shop/3d_shop_item.gd
@@ -0,0 +1,6 @@
+class_name ShopItem3D
+extends Node3D
+
+
+func _ready() -> void:
+	pass
